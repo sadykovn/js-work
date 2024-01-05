c17b85720f9d19861718ab0c31a96f86c4a90cc4
Nurbek <example@gmail.com>
2024-01-05 19:33:02 +0300

    default

---
+++ js/script.js
@@ -0,0 +1,109 @@
+'use strict'
+
+
+// #4 work
+
+// // Data type/s
+// console.log(8 / 0); // Infinity
+// console.log("Nurbek" * 8); // NaN
+
+// // String
+// const clientName = "Nurbek"
+
+// // Boolean
+// const isMarried = false
+// const isCircleEarth = true
+
+// // null
+// console.log(clientAge); //озгормо жок болсо null болот
+
+// // undefined 
+// let und;
+// console.log(und); // озгормодо маалымат жок болсо undefined болбот
+
+// // object
+// const thief = {
+//     age: 30,//number
+//     jacket: "black",//strihg
+//     isLong: false,//boolean
+// }
+// console.log(thief.age); // 30
+
+// // array
+// const colors = ["red", "black", "green", 10, [], {}];
+// console.log(colors[1]); // black
+
+
+
+
+
+//////////////////////////////
+// #5 work
+
+// alert("Hello world")
+
+// const place = confirm("Are you here?")
+// console.log(place);
+
+// const currentPlace = prompt("Where are you?")
+// console.log(currentPlace);
+
+// const age = +prompt("How old are you?")
+// console.log(age + 15);
+
+// const favouriteColor = [];
+
+// favouriteColor[0] = prompt("What's your favourite color #1")
+// favouriteColor[1] = prompt("What's your favourite color #2")
+// favouriteColor[2] = prompt("What's your favourite color #3")
+
+// console.log(favouriteColor);
+
+
+
+////////////////////////////
+// #6 work interpolation
+
+// const user = "Nurbek";
+// const job = "JavaScript developer";
+
+// console.log(`User name is ${user}, he is${job}`);
+
+
+
+
+////////////////////////
+// // 7 work operator
+
+// // incr / dicr
+
+// let incr = 10;
+// let dicr = 10;
+
+// console.log(incr++); // 10
+// console.log(dicr++); // 10
+
+// console.log(incr); // 11 
+// console.log(dicr); // 9
+
+// console.log(++incr); // 12
+// console.log(--dicr); // 8
+
+// console.log(11 % 4); // 3
+// console.log(10 % 3); // 1
+
+// console.log(5*5 == "25"); // true
+// console.log(5*5 === "25"); // false
+// console.log(5*5 === 25); // true
+
+// // && - жана || - же
+// const isAge = true;
+// const isClose = true;
+// const isChecked = false;
+
+// console.log(isAge && isClose && isChecked); // false
+// console.log(isAge && isClose && !isChecked); // true
+// console.log(isAge || isClose || isChecked); // true
+
+// console.log(2 + 2 * 2 !== 6); // false
+// console.log(2 + 2 * 2 !== "6"); // true
